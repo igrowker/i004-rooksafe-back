4ef0a0104e1ca7a4f67bec4f6274fd3332602bdb
Excelsior <ericchristensen59@gmail.com>
2024-11-14 14:11:05 -0300

    Update requirements.txt djangorestf

--- rooksafe/site_app/settings/local.py
+++ rooksafe/site_app/settings/local.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-tdbajof^6om84qix%vxin+9hes2@^i$1@s%xu^bkh4umy$r#(g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
