8f9ae32f76840d9a46e3a5f3c2c5552ffb5f34a4
Ariel Lima <ariellp.cuba@gmail.com>
2024-11-21 15:57:08 -0500

    Agregado soporte para docker

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#limpia primero el servicio anterior
+docker-compose down -v
+
+# hace un rebuild
+docker-compose build
+
+#Ejecuta los servicios
+docker-compose up -d
+
+
+#Ejecuta las migraciones
+docker-compose exec -it rooksafe-back python3 manage.py collectstatic --noinput
+docker-compose exec -it rooksafe-back python3 manage.py makemigrations
+
+docker-compose exec -it rooksafe-back python3 manage.py migrate
+#Crea superusuario
+docker-compose exec -it rooksafe-back python3 manage.py  createsuperuser --noinput
