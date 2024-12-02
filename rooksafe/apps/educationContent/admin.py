ce26f611151fa616a638519c4e6a98dfbc51ff43
Angie <eguzkia6@gmail.com>
2024-12-03 00:01:35 +0100

    updated admin.py for educationContent

--- rooksafe/apps/educationContent/admin.py
+++ rooksafe/apps/educationContent/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import EducationContent
 # Register your models here.
+
+@admin.register(EducationContent)
+class EducationContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content_type', 'level', 'created_at')
+    list_filter = ('content_type', 'level', 'created_at')
+    search_fields = ('title',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at' 
